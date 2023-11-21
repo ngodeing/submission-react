a12256f8b4ffbc6d970f90a2778e3cdec021fe1b
Taufik Nurrohman <onlytaufik47@gmail.com>
2023-11-21 18:27:29 +0700

    Set Up

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+function Form(){
+    return (
+        <div>
+            <form>
+                <label>Masukkan Judul</label>
+                <input type="text" />
+                <button type="submit">Tambah</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
